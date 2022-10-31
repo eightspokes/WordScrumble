ef3a09141bd0c3ceda38cae591ac05cfeb75e051
romanmuni8 <romanmuni8@gmail.com>
2022-10-31 13:02:18 -0400

    Initial Commit

---
+++ WordScrumble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScrumble
+//
+//  Created by Roman on 10/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScrumble/WordScrumbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrumbleApp.swift
+//  WordScrumble
+//
+//  Created by Roman on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrumbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
